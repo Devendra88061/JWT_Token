acb24ff722b20a7696a37564aec54f5c930ad2e8
Devendra Patil <devendra.patil@virtoustack.com>
2023-10-29 18:53:34 +0530

    update in get all users api

--- modules/user/user.service.ts
+++ modules/user/user.service.ts
@@ -2,35 +2,35 @@ import CrudOperations from "../../common/db/crud";
 import Users from "../../models/user";
 import _ from "lodash";
 
-class userService{
+class userService {
 
-    public static async getAllUser( next: CallableFunction) {
+    public static async getAllUser(next: CallableFunction) {
         try {
             let userCount = await new CrudOperations(Users).countAllDocuments({});
-            let userData = await new CrudOperations(Users).getAllDocuments({},{},{limit:0, pageNo:0},{});
+            let userData = await new CrudOperations(Users).getAllDocuments({}, {}, { limit: 0, pageNo: 0 }, {});
 
             const finalResult = {
-                count : userCount,
-                result : userData,
+                count: userCount,
+                result: userData,
             }
-            next(null, finalResult);         
+            next(null, finalResult);
         }
         catch (err: any) {
             return next(err, "Something went wrong!");
         }
     }
 
-    public static async getUserById(userId : any, next: CallableFunction) {
+    public static async getUserById(userId: any, next: CallableFunction) {
         try {
-            let userData = await new CrudOperations(Users).getDocumentById({_id: userId},{});
-            next(null, userData);         
+            let userData = await new CrudOperations(Users).getDocumentById({ _id: userId }, {});
+            next(null, userData);
         }
         catch (err: any) {
             return next(err, "Something went wrong!");
         }
     }
 
-    public static async updateUserById(userId : any, userDoc: Record<any, any>, next: CallableFunction) {
+    public static async updateUserById(userId: any, userDoc: Record<any, any>, next: CallableFunction) {
         try {
             const oldUser = await new CrudOperations(Users).getDocument({ _id: userId }, {});
             const newUser = _.extend(oldUser, userDoc);
@@ -38,9 +38,22 @@ class userService{
             await new CrudOperations(Users).save(newUser).then((result: any) => {
                 next(null, result);
             }).catch((error: any) => { next(error); });
-    
+
         }
         catch (err: any) {
+            return next(err, "Something went wrong!");
+        }
+    }
+
+    public static async deleteUserById(userId: any, next: CallableFunction) {
+        try {
+            const deletedUser = await new CrudOperations(Users).getDocumentById({ _id: userId }, { isDeleted: true });
+            if (deletedUser) {
+                next(null, deletedUser);
+            } else {
+                next("No User Found To Delete!");
+            }
+        } catch (err: any) {
             return next(err, "Something went wrong!");
         }
     }
