c8b6d01038d5b130374baf9f32b05901c43753e2
Devendra Patil <devendra.patil@virtoustack.com>
2023-10-29 18:54:09 +0530

    remove extra spaces from user controller

--- modules/user/user.controller.ts
+++ modules/user/user.controller.ts
@@ -3,60 +3,76 @@ import HttpException from "../../common/db/http.Exception/http.Exception";
 import HttpResponse from "../../common/db/http.Response/http.Response";
 import userService from "./user.service";
 
+class userController {
+    
+  // Get All users
+  public static async getAllUsers(request: Request, response: Response, next: NextFunction) {
+    try {
+      userService.getAllUser((err: any, result: any) => {
+        if (err) {
+          next(new HttpException(400, err));
+        } else {
+          response.status(200).send(new HttpResponse(null, result, "Get All Users", null, null, null));
+        }
+      });
+    }
+    catch (err) {
+      next(new HttpException(400, "Something went wrong"));
+    }
+  }
+
+  // get userById
+  public static async getUserById(request: Request, response: Response, next: NextFunction) {
+    try {
+      const userId = request.params.id;
+      userService.getUserById(userId, (err: any, result: any) => {
+        if (err) {
+          next(new HttpException(400, err));
+        } else {
+          response.status(200).send(new HttpResponse(null, result, "Get Single User", null, 1, null));
+        }
+      });
+    }
+    catch (err) {
+      next(new HttpException(400, "Something went wrong"));
+    }
+  }
+
+  // updateUserById 
+  public static async updateUserById(request: Request, response: Response, next: NextFunction) {
+    try {
+      const userId = request.params.id;
+      const updatedUser = request.body;
+      userService.updateUserById(userId, updatedUser, (err: any, result: any) => {
+        if (err) {
+          next(new HttpException(400, err));
+        } else {
+          response.status(200).send(new HttpResponse(null, result, "Update User", null, 1, null));
+        }
+      });
+    }
+    catch (err) {
+      next(new HttpException(400, "Something went wrong"));
+    }
+  }
 
 
-class userController{
-
-    // Get All users
-    public static async getAllUsers(request: Request, response: Response, next: NextFunction) {
-        try {
-          userService.getAllUser((err: any, result: any) => {
-            if (err) {
-              next(new HttpException(400, err));
-            } else {
-              response.status(200).send(new HttpResponse(null, result, "Get All Users", null, null, null));
-            }
-          });
+  // Delete user by ID
+  public static async deleteUserById(request: Request, response: Response, next: NextFunction) {
+    try {
+      const userId = request.params.id;
+      userService.deleteUserById(userId, (err: any, result: any) => {
+        if (err) {
+          next(new HttpException(400, err));
+        } else {
+          response.status(200).send(new HttpResponse(null, result, "Delete User", null, 1, null));
         }
-        catch (err) {
-          next(new HttpException(400, "Something went wrong"));
-        }
-      }
-
-      // get userById
-      public static async getUserById(request: Request, response: Response, next: NextFunction) {
-        try {
-            const userId = request.params.id;
-          userService.getUserById(userId, (err: any, result: any) => {
-            if (err) {
-              next(new HttpException(400, err));
-            } else {
-              response.status(200).send(new HttpResponse(null, result, "Get Single User", null, 1, null));
-            }
-          });
-        }
-        catch (err) {
-          next(new HttpException(400, "Something went wrong"));
-        }
-      }
-
-    // updateUserById 
-      public static async updateUserById(request: Request, response: Response, next: NextFunction) {
-        try {
-            const userId = request.params.id;
-            const updatedUser = request.body;
-          userService.updateUserById(userId,updatedUser, (err: any, result: any) => {
-            if (err) {
-              next(new HttpException(400, err));
-            } else {
-              response.status(200).send(new HttpResponse(null, result, "Update User", null, 1, null));
-            }
-          });
-        }
-        catch (err) {
-          next(new HttpException(400, "Something went wrong"));
-        }
-      }
+      });
+    }
+    catch (err) {
+      next(new HttpException(400, "Something went wrong"));
+    }
+  }
 
 
 }
